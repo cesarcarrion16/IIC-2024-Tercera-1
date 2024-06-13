e213f1df25924a9d0476122032570c1f81838adc
cesarcarrion16 <leonardocarrion184@gmail.com>
2024-06-12 21:40:44 -0600

    Primera clase

---
+++ parte_superior.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de Tecera 1" content="Pagina de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+            <img src="./images/hospital.jpg" alt="">
+        </a>
+        
+        <p class="texto-menu-izquierda"> 
+            El hospital
+            
+        </p>
+    </div>
+</menu>
+
+</html>
