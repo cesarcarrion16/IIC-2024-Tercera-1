d923b0a42a487f2c96ca7f00cd845e4f51bdbff7
cesarcarrion16 <leonardocarrion184@gmail.com>
2024-06-26 21:20:05 -0600

    Accordion

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<?php require_once "./parte_superior.php"?>
+<?php require_once "./parte_superior.php" ?>
 
 
 <div class="titulo">
-<p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
+    <p>Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?</p>
 
 </div>
+
 <body>
     <div class="main-img">
         <img src="./images/Clases-en-linea-1.png" alt="">
-    
     </div>
+    <div class = "main-accordion-container"></div>
+
+
 </body>
 
 
-<?php require_once "./parte_Inferior.php"?>
+<?php require_once "./parte_Inferior.php" ?>
--- parte_inferior.php
+++ parte_inferior.php
@@ -1,1 +1,3 @@
 <script src="./script/hamburger-icon.js"></script>
+
+<script src="./script/accordion.js"></script>
